3c3883b26845d7f69acb228e47ec655f0fb3de78
Alex.Liu <alexliu13483@gmail.com>
2016-02-23 18:57:23 +0800

    Add DetermineCompoundIntest test Move ComputingInterest.c to 12_13_ComputingInterest

---
+++ ExercisesForProgrammersInC/tests/13_DetermineCompoundInterest/DetermineCompoundInterestTest.cpp
@@ -0,0 +1,43 @@
+/*
+ * DetermineCompoundInterestTest.cpp
+ *
+ *  Created on: 2016¦~2¤ë23¤é
+ *      Author: Alex.Liu
+ */
+
+/* Useful CppUTest Assert Functions
+ * 	STRCMP_EQUAL(expect, result);
+ * 	LONGS_EQUAL(expect, result);
+ * 	BYTES_EQUAL(expect, result);
+ * 	POINTERS_EQUAL(expect, result);
+ *  DOUBLES_EQUAL(expected,actual,threshold);
+ */
+
+
+
+extern "C"
+{
+//#include "<<MODULE_HEAD_FILE>>.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(CompoundInterestTest)
+{
+
+    void setup()
+    {
+    	
+    }
+
+    void teardown()
+    {
+
+    }
+
+};
+
+TEST(CompoundInterestTest, FirstTest)
+{
+	FAIL("Start a initial test...");
+}
