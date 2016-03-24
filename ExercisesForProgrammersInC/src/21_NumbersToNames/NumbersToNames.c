02778eb271543ebb0b7636e2a1f4a405d7bd054d
Alex.Liu <alexliu13483@gmail.com>
2016-03-24 17:14:53 +0800

    Modify 21_NumbersToNames.c

--- ExercisesForProgrammersInC/src/21_NumbersToNames/NumbersToNames.c
+++ ExercisesForProgrammersInC/src/21_NumbersToNames/NumbersToNames.c
@@ -5,52 +5,17 @@
  *      Author: Alex.Liu
  */
 
+const char * dictionaryMonth[12] = {
+		"January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December"
+};
 
 void NumbersToNames_create() {
 }
 
 char * NumbersToNames_convertNumberToMonth(int num) {
-	char * strMonth = "";
+	if (num < 1 || num > 12)
+		return "";
 
-	switch (num) {
-	case 1:
-		strMonth = "January";
-		break;
-	case 2:
-		strMonth = "February";
-		break;
-	case 3:
-		strMonth = "March";
-		break;
-	case 4:
-		strMonth = "April";
-		break;
-	case 5:
-		strMonth = "May";
-		break;
-	case 6:
-		strMonth = "June";
-		break;
-	case 7:
-		strMonth = "July";
-		break;
-	case 8:
-		strMonth = "August";
-		break;
-	case 9:
-		strMonth = "September";
-		break;
-	case 10:
-		strMonth = "October";
-		break;
-	case 11:
-		strMonth = "November";
-		break;
-	case 12:
-		strMonth = "December";
-		break;
-	default:
-		break;
-	}
-	return strMonth;
+	return (char *)dictionaryMonth[num - 1];
 }
