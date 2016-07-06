d4c1856257c22c119747ac7638c01e660093004e
Alex.Liu <alexliu13483@gmail.com>
2016-07-06 09:58:06 +0800

    Add comments in ConsoleIO.h

--- ExercisesForProgrammersInC/include/Common/ConsoleIO.h
+++ ExercisesForProgrammersInC/include/Common/ConsoleIO.h
@@ -3,6 +3,9 @@
  *
  *  Created on: 2016¦~3¤ë4¤é
  *      Author: Alex.Liu
+ *
+ *  This module provides a interface that groups the APIs which access to the console.
+ *  The purpose of this module is to do unit tests automatically with test doubles.
  */
 
 #ifndef INCLUDE_COMMON_CONSOLEIO_H_
